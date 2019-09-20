32ba30acaec888dcb68281f85b6250b13ec8e4f7
arc0801 <54796547+arc0801@users.noreply.github.com>
2019-09-20 10:10:15 +0900

    jumin done

--- src/com/arc/t1/Test2.java
+++ src/com/arc/t1/Test2.java
@@ -14,22 +14,52 @@ public class Test2 {
 		// 결과값 == 검증용 번호
 		
 		Scanner sc = new Scanner(System.in);
-		
+		System.out.println("주민등록번호를 입력하세요!");
 		String num = "971222-1234567";
 		num = num.replace("-", "");
 		int sum = 0;
+		int idx =2;
+		/*
+		//1.split
+		String [] nums = num.split("");
 		
-		
-		for(int i=0;i<num.length()-1;i++) {
-			char a = num.charAt(i);
-			String b = num.valueOf(a);
-			sum = sum + Integer.parseInt(b)*(2+i);
-			System.out.println(sum);
-			
+		//2. substring
+		for(int j=0;j<num.length()-1;j++) {
+			String n1 = num.substring(j, j+1);
+					System.out.println(n1);
 		}
 		
+		*/
+		
+		//3. charAt
+		for(int i=0;i<num.length()-1;i++) {
+			char a = num.charAt(i);
+			String b = String.valueOf(a);
+			sum = sum + Integer.parseInt(b)*idx;
+			//if(i==6) {
+			//	continue;
+			//}
+			idx++;
+			if(idx==10) {
+				idx=2;
+			}
+		}
+		
+		sum=sum%11;
+		sum = 11-sum;
+		
+		if(sum>9) {
+			sum = sum%10;
+		}
+		
+		int result = Integer.parseInt(String.valueOf(num.charAt(12)));
 		
 		
+		if(sum==result) {
+			System.out.println("등록된 주민번호입니다");
+		}else {
+			System.out.println("철컹철컹");
+		}
 		
 		
 		
